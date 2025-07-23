f8273c0d3e16c7c4b324e8c9bb0ff70fc612861a
grabcoding <programmers472@gmail.com>
2025-07-23 10:21:17 +0530

    component injector is working fine

--- src/app/dashboard/graph-component/graph-component.ts
+++ src/app/dashboard/graph-component/graph-component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-graph-component',
+  standalone:true,
   imports: [],
   templateUrl: './graph-component.html',
   styleUrl: './graph-component.css'
 })
 export class GraphComponent {
+  @Input()  apple:string='';
 
 }
